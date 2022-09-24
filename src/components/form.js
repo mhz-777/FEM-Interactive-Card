cc1d426f9bccae2130e52a20ac1180e023893718
Mustafa <mhassanzadeh777@gmail.com>
2022-09-23 21:22:07 -0600

    Reworked form using react-hook-form library

--- src/components/form.js
+++ src/components/form.js
@@ -2,9 +2,12 @@ import './form.css';
 import Cards from './cards';
 import Confirmed from './confirmed';
 import {useState} from "react";
+import {useForm} from "react-hook-form";
 
 const Form = () => {
 
+    const {register, handleSubmit, formState: {errors}} = useForm();
+    const [submitted, setStatus] = useState();
     const [data, setData] = useState ({
         name: "",
         number: "",
@@ -13,30 +16,16 @@ const Form = () => {
         cvc: ""
     });
 
-    const [submitted, setStatus] = useState();
+    const formSubmit = (event) => {
+        setStatus(true);
+    }
 
-    const [invalid, setValidity] = useState();
-
+    
     function handleChange(event) {
         setData({...data, [event.target.name]: event.target.value});
     };
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        setStatus(true);
-        
-    }
-
-    
-    function handleInvalid(event, name) {
-        event.preventDefault();
-        setValidity(true);
-
-       
-    }
    
-   
-
 
     return (
         <div className="main-container">
@@ -51,76 +40,117 @@ const Form = () => {
                 />
             </div>
             <div className="form-container">
-                <form onSubmit={handleSubmit} className="dataform" autoComplete='off'>
+                <form onSubmit={handleSubmit(formSubmit)} autoComplete='off'>
                     <div className="column-container">
-                            <label htmlFor="cardholdername" className="holder-name">
-                                CARDHOLDER NAME
-                                <input 
-                                type="text" 
-                                name="name"
-                                placeholder="e.g. Jane Appleseed" 
-                                pattern="^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$"
-                                onChange={handleChange}
-
-                                required
+                        <label htmlFor="cardholdername" className='holder-name'>
+                        CARDHOLDER NAME
+                            <input 
+                                    {...register("name", {
+                                        required: "Can't be blank",
+                                        minLength: {
+                                            value: 2,
+                                            message: "No."
+                                        },
+                                        pattern: {
+                                            value:/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/,
+                                            message: 'Wrong format, letters only'
+                                        }
+                                    })}
+                                    onChange={handleChange}
+                                    placeholder="e.g. Jane Appleseed" 
+                                    maxLength={30}
                                 />
-                                {invalid === true && <p>test</p>}
-                            </label>
-                            <label htmlFor="cardnumber" className="card-number">
-                                CARD NUMBER
-                                <input
-                                type="text" 
-                                name="number"
-                                placeholder='e.g. 1234 5678 9123 0000' 
-                                pattern='^([0-9]+( [0-9]+)+)'
-                                onChange={handleChange}
-                                maxLength={19}
-                                required
-                                />
-                                
-                            </label>
+                                <p className="error-message">{errors.name?.message}</p>
+                        </label>
+                        <label htmlFor="cardnumber" className='card-number'>
+                        CARD NUMBER               
+                            <input
+                                    {...register("number", {
+                                        required: "Can't be blank",
+                                        maxLength: {
+                                            value: 19,
+                                            message: 'Please match format'
+                                        },
+                                        pattern: {
+                                            value:/^([0-9]+( [0-9]+)+)/,
+                                            message: 'Wrong format, numbers only'
+                                        }
+                                    })}
+                                    onChange={handleChange}
+                                    placeholder='e.g. 1234 5678 9123 0000' 
+                                    maxLength={19}
+                            />
+                            <p className="error-message">{errors.number?.message}</p>
+                        </label>
                     </div>
                     <div className="row-container">
-                            <label htmlFor="expdate" className="expiry-date">
-                                EXP. DATE (MM/YY)
-                                <div className="input-container">
-                                    <input
-                                    type="text"
-                                    name="month"
-                                    placeholder="MM" 
-                                    pattern='^(0?[1-9]|1[012])$'
+                        <label htmlFor="expdate" className='expiry-date'>
+                        EXP. DATE (MM/YY)
+                        <div className="input-container">
+                            <input
+                                    {...register("month", {
+                                        required: "Can't be blank",
+                                        maxLength: {
+                                            value: 2,
+                                            message: 'Please match format'
+                                        },
+                                        pattern: {
+                                            value: /^(0?[1-9]|1[012])$/,
+                                            message: 'Wrong format, numbers only'
+                                        }
+                                    })}
                                     onChange={handleChange}
+                                    placeholder='MM'
+                                    type='text'
                                     maxLength={2}
-                                    required
-                                    />
-                                    <input
-                                    type="text" 
-                                    name="year"
-                                    placeholder="YY"
                                     
+                            />
+                            <input
+                                    {...register("year", {
+                                        required: "Can't be blank",
+                                        maxLength: {
+                                            value: 2,
+                                            message: 'Please match format'
+                                        },
+                                        pattern: {
+                                            value: /[0-9]+/,
+                                            message: "Wrong format, numbers only"
+                                        }
+                                    })}
                                     onChange={handleChange}
+                                    placeholder='YY'
                                     maxLength={2}
-                                    required
-                                    />
-                                </div>
-                            </label>
-                            <label htmlFor="cvc" className="cvc">
-                                CVC
-                                <input
-                                type="text" 
-                                name="cvc"
-                                placeholder="e.g 123" 
-                                
+                             />
+                        </div> 
+                        <p className="error-message">{errors.month?.message || errors.year?.message}</p>      
+                        </label>
+                        <label htmlFor="cvc" className='cvc'>
+                            CVC
+                            <input
+                                {...register("cvc", {
+                                    required: "Can't be blank",
+                                    maxLength: {
+                                        value: 3,
+                                        message: "Max 3 numbers"
+                                    },
+                                    pattern: {
+                                        value: /[0-9]+/,
+                                        message: "Wrong format, numbers only"
+                                    }
+                                })}
                                 onChange={handleChange}
+                                placeholder='e.g 123'
                                 maxLength={3}
-                                required
-                                />
-                            </label>
+                                
+                             />
+                        <p className="error-message">{errors.cvc?.message}</p>
+                        </label>
                     </div>
                     <div className="button-container">
                         <button className="submit-btn">Confirm</button>
                     </div>
                     {submitted === true && <Confirmed />}
+
                 </form>
                 
             </div>
